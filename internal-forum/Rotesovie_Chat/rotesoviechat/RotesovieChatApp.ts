6562f2ffa310a912420092cc4bba5b242e6c686c
Alexis <vualexishn2110@gmail.com>
2026-02-24 18:05:44 +0100

    First Commit

---
+++ internal-forum/Rotesovie_Chat/rotesoviechat/RotesovieChatApp.ts
@@ -0,0 +1,15 @@
+import {
+    IAppAccessors,
+    ILogger,
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+
+export class RotesovieChatApp extends App {
+    constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
+        super(info, logger, accessors);
+    }
+    public async initialize(): Promise<void>{
+        console.log("Hello World!");
+    }
+}
